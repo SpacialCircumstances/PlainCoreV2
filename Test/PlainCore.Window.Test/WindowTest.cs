30697e8bc2e9af5bb4618570c77906727f65e9b3
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2018-03-12 18:52:25 +0100

    Add simple window test

---
+++ Test/PlainCore.Window.Test/WindowTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace PlainCore.Window.Test
+{
+    public class WindowTest
+    {
+        [Fact]
+        void TestCreate()
+        {
+            //Default window settings
+            var window1 = new Window();
+            Assert.True(window1.IsOpen);
+            window1.Title = "Test";
+            window1.Close();
+        }
+
+        [Fact]
+        void TestLoop()
+        {
+            //Should not crash
+            var window1 = new Window(640, 480, "Test", true);
+            var counter = 0;
+            while (window1.IsOpen)
+            {
+                counter++;
+                if (counter > 50) window1.Close();
+                window1.PollEvents();
+                window1.Display();
+            }
+        }
+    }
+}
