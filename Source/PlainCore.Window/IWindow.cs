3d2ec4c1673435c9e36cc5680e6c16f0d41b7b40
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2018-03-12 18:30:41 +0100

    Add simple window

---
+++ Source/PlainCore.Window/IWindow.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Numerics;
+
+namespace PlainCore.Window
+{
+    public interface IWindow: IDisposable
+    {
+        bool IsOpen { get; }
+        string Title { get; set; }
+        uint Height { get; }
+        uint Width { get; }
+        bool IsResizable { get; }
+        bool IsVisible { get; }
+        void Close();
+        void PollEvents();
+        void Display();
+        Vector2 GetPosition();
+    }
+}
--- Source/PlainCore.Window/Window.cs
+++ Source/PlainCore.Window/Window.cs
@@ -1,10 +1,100 @@
-﻿using System;
+﻿using GLFWDotNet;
+using System;
 using System.Collections.Generic;
+using System.Numerics;
 using System.Text;
 
 namespace PlainCore.Window
 {
-    class Window
+    public class Window: IWindow
     {
+        public Window(uint width = 800, uint height = 600, string title = "PlainCore", bool resizable = false)
+        {
+            this.width = width;
+            this.height = height;
+            this.resizable = resizable;
+            this.title = title;
+            Open();
+        }
+
+        protected uint width;
+        protected uint height;
+        protected bool resizable;
+        protected bool visible = true;
+        protected string title;
+
+        public IntPtr Handle = IntPtr.Zero;
+
+        private bool Exists()
+        {
+            return GLFW.WindowShouldClose(Handle) == 0;
+        }
+
+        public bool IsOpen => Exists();
+
+        public string Title
+        {
+            get => title;
+            set
+            {
+                title = value;
+                GLFW.SetWindowTitle(Handle, title);
+            }
+        }
+
+        public uint Height => height;
+
+        public uint Width => width;
+
+        public bool IsResizable => resizable;
+
+        public bool IsVisible => visible;
+
+        public void Close()
+        {
+            GLFW.DestroyWindow(Handle);
+        }
+
+        public void Display()
+        {
+            GLFW.SwapBuffers(Handle);
+        }
+
+        public Vector2 GetPosition()
+        {
+            GLFW.GetWindowPos(Handle, out int x, out int y);
+            return new Vector2(x, y);
+        }
+
+        public void PollEvents()
+        {
+            GLFW.PollEvents();
+        }
+
+        protected virtual void Open()
+        {
+            if (GLFW.Init() == 0) throw new NotSupportedException("GLFW init failed");
+
+            //Set hints
+            GLFW.WindowHint(GLFW.CLIENT_API, GLFW.OPENGL_API);
+            GLFW.WindowHint(GLFW.OPENGL_PROFILE, GLFW.OPENGL_CORE_PROFILE);
+            GLFW.WindowHint(GLFW.CONTEXT_VERSION_MAJOR, 3);
+            GLFW.WindowHint(GLFW.CONTEXT_VERSION_MINOR, 3);
+
+            Handle = GLFW.CreateWindow((int)width, (int)height, title, IntPtr.Zero, IntPtr.Zero);
+            if(Handle == IntPtr.Zero)
+            {
+                GLFW.Terminate();
+                throw new NotSupportedException("Window creation failed");
+            }
+
+            GLFW.MakeContextCurrent(Handle);
+            //TODO: Callback handling?
+        }
+
+        public void Dispose()
+        {
+            GLFW.Terminate();
+        }
     }
 }
