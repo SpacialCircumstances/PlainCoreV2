998bced55909abc78c1a7c60e655eb2fcaf21f71
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2018-03-12 21:50:22 +0100

    Add OpenGL

---
+++ Source/PlainCore.Graphics.Core/OpenGLWindow.cs
@@ -0,0 +1,15 @@
+﻿using OpenGL;
+
+namespace PlainCore.Graphics.Core
+{
+    public class OpenGLWindow : Window.Window
+    {
+        public OpenGLWindow(uint width, uint height, string title, bool resizable) : base(width, height, title, resizable, new Window.ContextSettings(3, 3, true))
+        {
+        }
+
+        static OpenGLWindow() {
+            Gl.Initialize();
+        }
+    }
+}
