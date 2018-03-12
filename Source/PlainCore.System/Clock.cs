85a32a336bbdecc5087b1c500b06fb49a06984c0
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2018-03-12 16:30:48 +0100

    Add clock

---
+++ Source/PlainCore.System/Clock.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace PlainCore.System
+{
+    public class Clock
+    {
+        public Clock()
+        {
+
+        }
+
+        private Stopwatch stopwatch;
+
+        public void Start()
+        {
+            stopwatch.Start();
+        }
+
+        public void Stop()
+        {
+            stopwatch.Stop();
+        }
+
+        public void Reset()
+        {
+            stopwatch.Reset();
+        }
+
+        public TimeSpan Restart()
+        {
+            var elapsed = stopwatch.Elapsed;
+            stopwatch.Restart();
+            return elapsed;
+        }
+
+        public TimeSpan Elapsed
+        {
+            get => stopwatch.Elapsed;
+        }
+    }
+}
