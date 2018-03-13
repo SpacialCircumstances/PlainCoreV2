378a123228839cd0b2ba6259f372ff0b2627f670
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2018-03-13 20:23:04 +0100

    Add VertexArrayBuffer

---
+++ Source/PlainCore.Graphics.Core/VertexArrayBuffer.cs
@@ -0,0 +1,56 @@
+﻿using OpenGL;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlainCore.Graphics.Core
+{
+    public class VertexArrayBuffer<T>: IDeviceBuffer where T: struct
+    {
+        public VertexArrayBuffer(uint vertexSize, BufferUsage usage = BufferUsage.StreamDraw, int initialCapacity = 3)
+        {
+            vertices = new T[initialCapacity];
+            this.vertexSize = vertexSize;
+            this.usage = usage;
+
+            handle = Gl.GenBuffer();
+            Verify.VerifyResourceCreated(handle);
+        }
+
+        protected readonly BufferUsage usage;
+        protected uint vertexSize;
+        protected T[] vertices;
+        protected uint handle;
+
+        public T[] Vertices
+        {
+            get => vertices;
+            set
+            {
+                vertices = value;
+            }
+        }
+
+        public void Bind()
+        {
+            Gl.BindBuffer(BufferTarget.ArrayBuffer, handle);
+        }
+
+        public void CopyData()
+        {
+            Bind();
+            Gl.BufferData(BufferTarget.ArrayBuffer, (this.vertexSize * (uint)vertices.Length), this.vertices, usage);
+            Unbind();
+        }
+
+        public void Dispose()
+        {
+            Gl.DeleteBuffers(handle);
+        }
+
+        public void Unbind()
+        {
+            Gl.BindBuffer(BufferTarget.ArrayBuffer, 0);
+        }
+    }
+}
