71860b8b4d8a39e59f21d0485a5e4d76c88dff3c
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2018-03-13 20:40:25 +0100

    Add VertexPositionColor

---
+++ Source/PlainCore.Graphics.Core/VertexPositionColor.cs
@@ -0,0 +1,18 @@
+﻿using System.Numerics;
+using System.Runtime.InteropServices;
+
+namespace PlainCore.Graphics.Core
+{
+    [StructLayout(LayoutKind.Sequential)]
+    public struct VertexPositionColor
+    {
+        public Vector2 Position;
+        public Color4 Color;
+
+        public VertexPositionColor(Vector2 position, Color4 color)
+        {
+            Position = position;
+            Color = color;
+        }
+    }
+}
