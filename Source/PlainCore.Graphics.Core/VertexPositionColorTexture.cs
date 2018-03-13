0d4c4a600c34da8a14f1b77c515b161497209838
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2018-03-13 17:51:17 +0100

    Add vertex type

---
+++ Source/PlainCore.Graphics.Core/VertexPositionColorTexture.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace PlainCore.Graphics.Core
+{
+    [StructLayout(LayoutKind.Sequential)]
+    public struct VertexPositionColorTexture
+    {
+        public Vector2 Position;
+        public Color4 Color;
+        public Vector2 TextureCoordinates;
+
+        public VertexPositionColorTexture(Vector2 position, Color4 color, Vector2 textureCoordinates)
+        {
+            Position = position;
+            Color = color;
+            TextureCoordinates = textureCoordinates;
+        }
+    }
+}
