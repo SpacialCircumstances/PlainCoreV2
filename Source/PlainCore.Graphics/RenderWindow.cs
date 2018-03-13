798f71994a2eaa91f43f94b8e54f6c364779d5f6
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2018-03-13 16:56:04 +0100

    Add Graphics project and RenderWindow

---
+++ Source/PlainCore.Graphics/RenderWindow.cs
@@ -0,0 +1,14 @@
+﻿using PlainCore.Graphics.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlainCore.Graphics
+{
+    public class RenderWindow : OpenGLWindow
+    {
+        public RenderWindow(uint width = 800, uint height = 600, string title = "PlainCore", bool resizable = false) : base(width, height, title, resizable)
+        {
+        }
+    }
+}
