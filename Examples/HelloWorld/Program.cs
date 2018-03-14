08255e1aba19c75cb321028544645d825dcb371d
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2018-03-14 18:41:41 +0100

    Port example from OpenGL.Net

--- Examples/HelloWorld/Program.cs
+++ Examples/HelloWorld/Program.cs
@@ -9,19 +9,8 @@ namespace HelloWorld
     {
         public static void Main(string[] args)
         {
-            var window = new RenderWindow();
-            var clock = new Clock();
-            clock.Start();
-            while(window.IsOpen)
-            {
-                window.PollEvents();
-                if (clock.Elapsed.TotalSeconds > 1) clock.Restart();
-                window.Clear(new Color4((float)clock.Elapsed.TotalSeconds, 0f, 0f, 0f));
-
-                window.Display();
-            }
-
-            window.Dispose();
+            new Example().Run();
         }
+
     }
 }
