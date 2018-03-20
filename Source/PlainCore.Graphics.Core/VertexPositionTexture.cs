ebdf2c175f028ea1ae38393ae0e0aaaab09e2c2e
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2018-03-20 20:22:55 +0100

    Add PositionTexture vertex type

---
+++ Source/PlainCore.Graphics.Core/VertexPositionTexture.cs
@@ -0,0 +1,20 @@
+﻿using System.Numerics;
+using System.Runtime.InteropServices;
+
+namespace PlainCore.Graphics.Core
+{
+    [StructLayout(LayoutKind.Sequential)]
+    public struct VertexPositionTexture
+    {
+        public Vector2 Position;
+        public Vector2 TextureCoordinates;
+
+        public const uint Size = 16;
+
+        public VertexPositionTexture(Vector2 position, Vector2 textureCoordinates)
+        {
+            Position = position;
+            TextureCoordinates = textureCoordinates;
+        }
+    }
+}
