517e8bac173933cc51d1deddc22e7b3c900692b1
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2018-03-20 21:14:13 +0100

    Add Position3Texture vertex

---
+++ Source/PlainCore.Graphics.Core/VertexPosition3Texture.cs
@@ -0,0 +1,20 @@
+﻿using System.Numerics;
+using System.Runtime.InteropServices;
+
+namespace PlainCore.Graphics.Core
+{
+    [StructLayout(LayoutKind.Sequential)]
+    public struct VertexPosition3Texture
+    {
+        public Vector3 Position;
+        public Vector2 TextureCoordinates;
+
+        public const uint Size = 20;
+
+        public VertexPosition3Texture(Vector3 position, Vector2 textureCoordinates)
+        {
+            Position = position;
+            TextureCoordinates = textureCoordinates;
+        }
+    }
+}
