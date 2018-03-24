fc69aa6f7133961616479ca560275dd8b6fb4b26
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2018-03-24 16:46:03 +0100

    Add shader tests

---
+++ Test/PlainCore.Graphics.Test/DefaultShaderTest.cs
@@ -0,0 +1,44 @@
+﻿using PlainCore.Graphics.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace PlainCore.Graphics.Test
+{
+    public class DefaultShaderTest
+    {
+        [Fact]
+        public void TestCompile()
+        {
+            var window = new OpenGLWindow(200, 200, "Test", false);
+
+            new ShaderPipeline(
+                DefaultShader.FromType(typeof(VertexPositionColor), ShaderType.Vertex),
+                DefaultShader.FromType(typeof(VertexPositionColor), ShaderType.Fragment));
+
+            new ShaderPipeline(
+                DefaultShader.FromType(typeof(VertexPosition3Color), ShaderType.Vertex),
+                DefaultShader.FromType(typeof(VertexPosition3Color), ShaderType.Fragment));
+
+            new ShaderPipeline(
+                DefaultShader.FromType(typeof(VertexPositionTexture), ShaderType.Vertex),
+                DefaultShader.FromType(typeof(VertexPositionTexture), ShaderType.Fragment));
+
+            new ShaderPipeline(
+                DefaultShader.FromType(typeof(VertexPosition3Texture), ShaderType.Vertex),
+                DefaultShader.FromType(typeof(VertexPosition3Texture), ShaderType.Fragment));
+
+            new ShaderPipeline(
+                DefaultShader.FromType(typeof(VertexPositionColorTexture), ShaderType.Vertex),
+                DefaultShader.FromType(typeof(VertexPositionColorTexture), ShaderType.Fragment));
+
+            new ShaderPipeline(
+                DefaultShader.FromType(typeof(VertexPosition3ColorTexture), ShaderType.Vertex),
+                DefaultShader.FromType(typeof(VertexPosition3ColorTexture), ShaderType.Fragment));
+
+            window.Close();
+            window.Dispose();
+        }
+    }
+}
