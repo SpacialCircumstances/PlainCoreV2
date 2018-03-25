252f782cdbd6d99c5cd1c51817450523f8ddf9b7
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2018-03-25 11:58:44 +0200

    Simple framebuffer class

---
+++ Source/PlainCore.Graphics.Core/Framebuffer.cs
@@ -0,0 +1,49 @@
+﻿using OpenGL;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlainCore.Graphics.Core
+{
+    public class Framebuffer: IBindable
+    {
+        public static Framebuffer GetDefault()
+        {
+            return new Framebuffer(0, true);
+        }
+
+        protected Framebuffer(uint handle, bool isDefault = false)
+        {
+            this.isDefault = isDefault;
+            Handle = handle;
+        }
+
+        public Framebuffer()
+        {
+            Handle = Gl.GenFramebuffer();
+            Verify.VerifyResourceCreated(Handle);
+        }
+
+        public readonly uint Handle;
+        protected readonly bool isDefault;
+
+        public void Bind()
+        {
+            Gl.BindFramebuffer(FramebufferTarget.Framebuffer, Handle);
+        }
+
+        public void Dispose()
+        {
+            //Can not dispose default framebuffer because it is owned by the context
+            if (!isDefault)
+            {
+                Gl.DeleteFramebuffers(Handle);
+            }
+        }
+
+        public void Unbind()
+        {
+            Gl.BindFramebuffer(FramebufferTarget.Framebuffer, 0);
+        }
+    }
+}
