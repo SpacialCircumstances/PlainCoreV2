5e186232087550b579a119fa1062368097402087
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2018-03-30 13:41:33 +0200

    Add MemoryBuffer

---
+++ Source/PlainCore.System/MemoryBuffer.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace PlainCore.System
+{
+    public class MemoryBuffer : IDisposable
+    {
+        public MemoryBuffer(int size)
+        {
+            buffer = new byte[size];
+            writer = new BinaryWriter(stream = new MemoryStream(buffer));
+        }
+
+        protected byte[] buffer;
+        protected int written;
+        protected MemoryStream stream;
+        protected BinaryWriter writer;
+
+        public int BytesWritten => written;
+        public int Size => buffer.Length;
+        public byte[] Buffer => buffer;
+
+        public void Clear()
+        {
+            written = 0;
+            writer.Seek(0, SeekOrigin.Begin);
+        }
+
+        public void Write(int value)
+        {
+            writer.Write(value);
+            written += 4;
+        }
+
+        public void Write(float value)
+        {
+            writer.Write(value);
+            written += 4;
+        }
+
+        public void Write(byte value)
+        {
+            writer.Write(value);
+            written++;
+        }
+
+        public void Dispose()
+        {
+            writer.Close();
+            writer.Dispose();
+            stream.Dispose();
+        }
+    }
+}
