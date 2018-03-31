2e1a772d5db0aeb1e4ace134a093d4e936ce86c8
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2018-03-31 18:31:11 +0200

    Create data buffer

--- Source/PlainCore.Graphics.Core/DataBuffer.cs
+++ Source/PlainCore.Graphics.Core/DataBuffer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 
 namespace PlainCore.Graphics.Core
@@ -10,8 +11,50 @@ namespace PlainCore.Graphics.Core
         {
             this.buffer = buffer;
             this.buffer.CopyRawData(IntPtr.Zero, initialSize);
+            Clear();
         }
 
         private IDeviceBuffer<T> buffer;
+        public IDeviceBuffer<T> DeviceBuffer => buffer;
+
+        private MemoryStream stream;
+        private BinaryWriter writer;
+
+        private int written;
+        public int WrittenBytes => written;
+
+        public void Flush()
+        {
+            writer.Flush();
+            var data = stream.ToArray();
+            buffer.ReplaceData(data, IntPtr.Zero);
+        }
+
+        public void Clear()
+        {
+            stream = new MemoryStream();
+            writer = new BinaryWriter(stream);
+            written = 0;
+        }
+
+        public void Write(int value)
+        {
+            writer.Write(value);
+        }
+
+        public void Write(float value)
+        {
+            writer.Write(value);
+        }
+
+        public void Write(byte value)
+        {
+            writer.Write(value);
+        }
+
+        public void Write(byte[] values)
+        {
+            writer.Write(values);
+        }
     }
 }
