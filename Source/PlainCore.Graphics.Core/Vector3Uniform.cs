6e5b0a1209a2e2e4d10ce211367136fc81992fe1
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2018-04-02 12:17:06 +0200

    Add Vector3 uniform

---
+++ Source/PlainCore.Graphics.Core/Vector3Uniform.cs
@@ -0,0 +1,32 @@
+﻿using OpenGL;
+using System.Numerics;
+using System.Text;
+
+namespace PlainCore.Graphics.Core
+{
+    public class Vector3Uniform : IUniform
+    {
+        public Vector3Uniform(string name)
+        {
+
+        }
+
+        protected string name;
+        public string Name => name;
+
+        protected Vector3 vector;
+        public Vector3 Vector
+        {
+            get => vector;
+            set
+            {
+                vector = value;
+            }
+        }
+
+        public void Set(ShaderPipeline pipeline)
+        {
+            Gl.Uniform3f(pipeline.GetUniformLocation(name), 1, ref vector);
+        }
+    }
+}
