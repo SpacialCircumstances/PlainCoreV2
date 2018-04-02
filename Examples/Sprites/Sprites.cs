24c930f0aed94c0739953a76ea290a4061da53d8
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2018-04-02 11:14:45 +0200

    Second sprite

--- Examples/Sprites/Sprites.cs
+++ Examples/Sprites/Sprites.cs
@@ -2,6 +2,7 @@
 using PlainCore.Graphics.Core;
 using System;
 using System.Collections.Generic;
+using System.Numerics;
 using System.Text;
 
 namespace Sprites
@@ -10,6 +11,7 @@ namespace Sprites
     {
         SpriteBatch batch;
         Texture tex;
+        Texture tex2;
         Matrix4fUniform worldMatrix;
         public void Run()
         {
@@ -33,13 +35,16 @@ namespace Sprites
         {
             batch = new SpriteBatch();
             tex = Texture.FromFile("Example.png");
+            tex2 = Texture.FromFile("Screenshot.png");
             worldMatrix = new Matrix4fUniform(DefaultShader.MVP_UNIFORM_NAME);
+            worldMatrix.Matrix = Matrix4x4.CreateOrthographic(2, 2, -1, 1);
         }
 
         public void Draw()
         {
             batch.Begin(worldMatrix);
             batch.Draw(tex, Color4.White, 0f, 0, 1, 1);
+            batch.Draw(tex2, Color4.White, -0.5f, 0.5f, 0.5f, 0.5f);
             batch.End();
         }
     }
