7aaee8289ad07d2954df643e7278ea1220f60d17
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2018-04-02 11:56:18 +0200

    Add Vector2 uniform

---
+++ Source/PlainCore.Graphics.Core/Vector2Uniform.cs
@@ -0,0 +1,34 @@
+﻿using OpenGL;
+using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+
+namespace PlainCore.Graphics.Core
+{
+    public class Vector2Uniform : IUniform
+    {
+        public Vector2Uniform(string name)
+        {
+
+        }
+
+        protected string name;
+        public string Name => name;
+
+        protected Vector2 vector;
+        public Vector2 Vector
+        {
+            get => vector;
+            set
+            {
+                vector = value;
+            }
+        }
+
+        public void Set(ShaderPipeline pipeline)
+        {
+            Gl.Uniform2f(pipeline.GetUniformLocation(name), 1, ref vector);
+        }
+    }
+}
