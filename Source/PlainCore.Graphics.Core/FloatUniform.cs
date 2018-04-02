928c560509c0e5fbfc06d41169dfb58b0c36ceb4
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2018-04-02 11:39:12 +0200

    Add float uniform

---
+++ Source/PlainCore.Graphics.Core/FloatUniform.cs
@@ -0,0 +1,33 @@
+﻿using OpenGL;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlainCore.Graphics.Core
+{
+    public class FloatUniform : IUniform
+    {
+        public FloatUniform(string name)
+        {
+            this.name = name;
+        }
+
+        protected string name;
+        public string Name => name;
+
+        protected float value;
+        public float Value
+        {
+            get => value;
+            set
+            {
+                this.value = value;
+            }
+        }
+
+        public void Set(ShaderPipeline pipeline)
+        {
+            Gl.Uniform1f(pipeline.GetUniformLocation(name), 1, ref value);
+        }
+    }
+}
