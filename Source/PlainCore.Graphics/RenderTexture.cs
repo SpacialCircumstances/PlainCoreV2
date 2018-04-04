a6a80436897d27b3bd74514a9420f34e25c4b1be
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2018-04-04 19:33:54 +0200

    First tries for rendering to texture

---
+++ Source/PlainCore.Graphics/RenderTexture.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using PlainCore.Graphics.Core;
+using PlainCore.System;
+
+namespace PlainCore.Graphics
+{
+    public class RenderTexture : ITexture
+    {
+        public RenderTexture(int width, int height)
+        {
+            viewport = new Viewport(0, width, 0, height);
+            depthBuffer = new DepthBuffer(width, height);
+            framebuffer = new Framebuffer();
+            deviceTexture = new DeviceTexture(DefaultShader.DEFFAULT_TEXTURE_UNIFORM_NAME, width, height, false);
+            deviceTexture.Bind();
+            framebuffer.Bind();
+            deviceTexture.CopyRawData(IntPtr.Zero, 0);
+            framebuffer.AttachDepthBuffer(depthBuffer);
+            framebuffer.AttachTexture(deviceTexture);
+            deviceTexture.Unbind();
+            framebuffer.Check();
+            texture = Texture.FromDeviceTexture(deviceTexture);
+        }
+
+        protected DepthBuffer depthBuffer;
+        protected Viewport viewport;
+        protected Framebuffer framebuffer;
+        protected DeviceTexture deviceTexture;
+        protected Texture texture;
+        public Texture Texture => texture;
+
+        public FloatRectangle Rectangle => texture.Rectangle;
+    }
+}
---
+++ Test/PlainCore.Graphics.Test/RenderTextureTest.cs
@@ -0,0 +1,23 @@
+﻿using PlainCore.Graphics.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace PlainCore.Graphics.Test
+{
+    public class RenderTextureTest
+    {
+        [Fact]
+        public void TestCreate()
+        {
+            //Dummy window for context initialization
+            var window = new OpenGLWindow(200, 200, "Test", false);
+
+            var tex = new RenderTexture(800, 600);
+          
+            window.Close();
+            window.Dispose();
+        }
+    }
+}
