751a3b6a84b8fc962f4edd091e6c81c546ccf7c2
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2018-04-04 17:34:55 +0200

    Add Viewport

---
+++ Source/PlainCore.Graphics.Core/Viewport.cs
@@ -0,0 +1,25 @@
+﻿using OpenGL;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlainCore.Graphics.Core
+{
+    public class Viewport
+    {
+        protected int left, right, bottom, top;
+
+        public Viewport(int left, int right, int bottom, int top)
+        {
+            this.left = left;
+            this.right = right;
+            this.bottom = bottom;
+            this.top = top;
+        }
+
+        public void Set()
+        {
+            Gl.Viewport(left, bottom, right - left, top - bottom);
+        }
+    }
+}
