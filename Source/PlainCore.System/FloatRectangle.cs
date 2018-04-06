1ad983fd0f2a236c0a9c57dd7e352811146f20d8
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2018-04-06 19:17:40 +0200

    Add documentation to System library

--- Source/PlainCore.System/FloatRectangle.cs
+++ Source/PlainCore.System/FloatRectangle.cs
@@ -5,19 +5,37 @@ using System.Text;
 
 namespace PlainCore.System
 {
+    /// <summary>
+    /// A float rectangle.
+    /// </summary>
     public class FloatRectangle: IEquatable<FloatRectangle>
     {
+        /// <summary>
+        /// Create a float rectangle.
+        /// </summary>
+        /// <param name="position">The position of the rectangle</param>
+        /// <param name="size">The size of the rectangle</param>
         public FloatRectangle(Vector2 position, Vector2 size)
         {
             this.position = position;
             this.size = size;
         }
 
+        /// <summary>
+        /// Create a rectangle with the default Position & Size.
+        /// </summary>
         public FloatRectangle()
         {
 
         }
 
+        /// <summary>
+        /// Create a float rectangle.
+        /// </summary>
+        /// <param name="x">The x component of the rectangle</param>
+        /// <param name="y">The y component of the rectangle</param>
+        /// <param name="w">The width of the rectangle</param>
+        /// <param name="h">The height of the rectangle</param>
         public FloatRectangle(float x, float y, float w, float h): this(new Vector2(x, y), new Vector2(w, h))
         {
 
@@ -26,23 +44,37 @@ namespace PlainCore.System
         protected Vector2 position;
         protected Vector2 size;
 
+        /// <summary>
+        /// Gets or sets the position.
+        /// </summary>
         public Vector2 Position
         {
             get => position;
             set => position = value;
         }
 
+        /// <summary>
+        /// Gets or sets the size.
+        /// </summary>
         public Vector2 Size
         {
             get => size;
             set => size = value;
         }
 
+        /// <summary>
+        /// Gets the End of the rectangle. Is equal to Position + Size.
+        /// </summary>
         public Vector2 End
         {
             get => Position + Size;
         }
 
+        /// <summary>
+        /// Checks if two rectangles are equal.
+        /// </summary>
+        /// <param name="other">The other rectangle</param>
+        /// <returns>The equality of both rectangles</returns>
         public bool Equals(FloatRectangle other)
         {
             return position == other.position && size == other.size;
--- Source/PlainCore.System/MemoryBuffer.cs
+++ Source/PlainCore.System/MemoryBuffer.cs
@@ -6,8 +6,15 @@ using System.Text;
 
 namespace PlainCore.System
 {
+    /// <summary>
+    /// Allows writing primitives to a byte array
+    /// </summary>
     public class MemoryBuffer : IDisposable
     {
+        /// <summary>
+        /// Create a MemoryBuffer with the maximum size of the byte array
+        /// </summary>
+        /// <param name="size">The maximum size of the byte array backing the buffer</param>
         public MemoryBuffer(int size)
         {
             buffer = new byte[size];
@@ -19,34 +26,65 @@ namespace PlainCore.System
         protected MemoryStream stream;
         protected BinaryWriter writer;
 
+        /// <summary>
+        /// The amount of bytes written since creation/calling Clear().
+        /// </summary>
         public int BytesWritten => written;
+
+        /// <summary>
+        /// The maximum size of the byte array backing the buffer.
+        /// </summary>
         public int Size => buffer.Length;
+
+        /// <summary>
+        /// The buffer
+        /// </summary>
         public byte[] Buffer => buffer;
 
+        /// <summary>
+        /// Clears the stream so writing begins from the start.
+        /// </summary>
+        /// <remarks>
+        /// Does not reallocate the buffer</remarks>
         public void Clear()
         {
             written = 0;
             writer.Seek(0, SeekOrigin.Begin);
         }
 
+        /// <summary>
+        /// Write an Integer to the buffer.
+        /// </summary>
+        /// <param name="value">Value to write</param>
         public void Write(int value)
         {
             writer.Write(value);
             written += 4;
         }
 
+        /// <summary>
+        /// Write an Single to the buffer.
+        /// </summary>
+        /// <param name="value">Value to write</param>
         public void Write(float value)
         {
             writer.Write(value);
             written += 4;
         }
 
+        /// <summary>
+        /// Write an Byte to the buffer.
+        /// </summary>
+        /// <param name="value">Value to write</param>
         public void Write(byte value)
         {
             writer.Write(value);
             written++;
         }
 
+        /// <summary>
+        /// Dispose the resources used by this instance.
+        /// </summary>
         public void Dispose()
         {
             writer.Close();
