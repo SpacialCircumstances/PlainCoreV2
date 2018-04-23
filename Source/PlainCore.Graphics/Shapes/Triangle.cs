d67dfdb4f53a067852f52d58448bcdd4a96602f2
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2018-04-23 18:59:14 +0200

    Add simple triangle shape

---
+++ Source/PlainCore.Graphics/Shapes/Triangle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using PlainCore.Graphics.Core;
+
+namespace PlainCore.Graphics.Shapes
+{
+    public class Triangle : IShape
+    {
+        public Triangle(VertexPositionColor[] vertices)
+        {
+            if (vertices.Length != 3) throw new ArgumentOutOfRangeException();
+            this.vertices = vertices;
+        }
+
+        private readonly VertexPositionColor[] vertices;
+        private readonly static int[] INDICES = { 0, 1, 2 };
+
+        public int[] GetIndices()
+        {
+            return INDICES;
+        }
+
+        public VertexPositionColor[] GetVertices()
+        {
+            return vertices;
+        }
+    }
+}
