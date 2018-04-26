15ef58e00b37aca73ed27167085c4f75fdf0441a
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2018-04-26 11:06:48 +0200

    Add free shape

---
+++ Source/PlainCore.Graphics/Shapes/FreeShape.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using PlainCore.Graphics.Core;
+
+namespace PlainCore.Graphics.Shapes
+{
+    public class FreeShape : IShape
+    {
+        public FreeShape(int[] indices, VertexPositionColor[] vertices)
+        {
+            Indices = indices;
+            Vertices = vertices;
+        }
+
+        public int[] Indices { get; set; }
+        public VertexPositionColor[] Vertices { get; set; }
+
+        public int[] GetIndices()
+        {
+            return Indices;
+        }
+
+        public VertexPositionColor[] GetVertices()
+        {
+            return Vertices;
+        }
+    }
+}
