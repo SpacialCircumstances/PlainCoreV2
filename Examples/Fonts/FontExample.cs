0e7c07e66b222e883cbb473daf380169207978a3
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2018-04-27 20:56:34 +0200

    Add font generation example

---
+++ Examples/Fonts/FontExample.cs
@@ -0,0 +1,14 @@
+﻿using PlainCore.Graphics.Text;
+using SixLabors.ImageSharp;
+
+namespace Fonts
+{
+    public class FontExample
+    {
+        public void Run()
+        {
+            var description = new FontGenerator().GenerateFont("OpenSans-Regular.ttf", 40);
+            description.Bitmap.Save("Font.png");
+        }
+    }
+}
---
+++ Examples/Fonts/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Fonts
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            new FontExample().Run();
+        }
+    }
+}
