2ca16325ca945520857cf021c5a4b03324293289
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2018-05-02 18:40:39 +0200

    Add dynamic display list

---
+++ Source/PlainCore.Graphics/DynamicDisplayList.cs
@@ -0,0 +1,56 @@
+﻿using PlainCore.Graphics.Core;
+
+namespace PlainCore.Graphics
+{
+    public class DynamicDisplayList<T> : AbstractDisplayList<T> where T : struct
+    {
+        private readonly VertexArrayBuffer<T> vertexArrayBuffer;
+        private readonly IndexBuffer<T> indexBuffer;
+        private readonly VertexArrayObject<T> vertexArrayObject;
+
+        //Current indices count
+        private int elements;
+
+        protected DynamicDisplayList(uint vertexSize, OpenGL.PrimitiveType primitiveType = OpenGL.PrimitiveType.Triangles, ShaderPipeline pipeline = null, VertexAttributeDescription[] vertexAttributes = null) : base(pipeline, vertexAttributes)
+        {
+            vertexArrayBuffer = new VertexArrayBuffer<T>(vertexSize, OpenGL.BufferUsage.StaticDraw, primitiveType);
+            indexBuffer = new IndexBuffer<T>(OpenGL.BufferUsage.StaticDraw);
+            vertexArrayObject = new VertexArrayObject<T>(vertexArrayBuffer, this.pipeline, vertexAttributes);
+        }
+
+        public void SetVertices(T[] vertices)
+        {
+            vertexArrayBuffer.Bind();
+            vertexArrayBuffer.CopyData(vertices);
+            vertexArrayBuffer.Unbind();
+        }
+
+        public void SetIndices(int[] indices)
+        {
+            elements = indices.Length;
+            indexBuffer.Bind();
+            indexBuffer.CopyData(indices);
+            indexBuffer.Unbind();
+        }
+
+        public override void Draw(IResourceSet resourceSet)
+        {
+            pipeline.Bind();
+            vertexArrayObject.Bind();
+            indexBuffer.Bind();
+            vertexArrayBuffer.Bind();
+
+            foreach (var uniform in resourceSet.GetUniforms())
+            {
+                uniform.Set(pipeline);
+            }
+
+            indexBuffer.DrawIndexed(vertexArrayBuffer, elements);
+
+            vertexArrayBuffer.Unbind();
+            indexBuffer.Unbind();
+            vertexArrayObject.Unbind();
+            pipeline.Unbind();
+        }
+    }
+}
