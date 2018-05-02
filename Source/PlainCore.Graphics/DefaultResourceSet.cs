46b0b61035432722a89a1d9db3d6b58e90cc8ec9
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2018-05-02 17:45:14 +0200

    Add IResourceSet implementations

---
+++ Source/PlainCore.Graphics/DefaultResourceSet.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using PlainCore.Graphics.Core;
+
+namespace PlainCore.Graphics
+{
+    public class DefaultResourceSet : IResourceSet
+    {
+        public DefaultResourceSet(IRenderTarget renderTarget)
+        {
+            RenderTarget = renderTarget;
+        }
+
+        public IRenderTarget RenderTarget { get; }
+        public List<IUniform> Uniforms { get; set; } = new List<IUniform>();
+        private readonly Matrix4fUniform worldMatrixUniform = new Matrix4fUniform(DefaultShader.MVP_UNIFORM_NAME);
+
+        public virtual IEnumerator<IUniform> GetUniforms()
+        {
+            if (RenderTarget == null) throw new ArgumentNullException("RenderTarget");
+            worldMatrixUniform.Matrix = RenderTarget.WorldMatrix;
+
+            yield return worldMatrixUniform;
+
+            foreach (var uniform in Uniforms)
+            {
+                yield return uniform;
+            }
+        }
+    }
+}
---
+++ Source/PlainCore.Graphics/StaticDisplayList.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlainCore.Graphics
+{
+    public class StaticDisplayList : IDisplayList
+    {
+        public void Draw(IResourceSet resourceSet)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
