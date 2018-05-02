3dccdee65c8039f71e0c885b473e826110e25439
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2018-05-02 18:08:36 +0200

    Add AbstractDisplayList

---
+++ Source/PlainCore.Graphics/AbstractDisplayList.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlainCore.Graphics.Core
+{
+    public abstract class AbstractDisplayList<T> : IDisplayList where T: struct
+    {
+        protected ShaderPipeline pipeline;
+        protected readonly VertexAttributeDescription[] vertexAttributes;
+
+        protected AbstractDisplayList(ShaderPipeline pipeline = null, VertexAttributeDescription[] vertexAttributes = null)
+        {
+            this.pipeline = pipeline ?? new ShaderPipeline(
+                DefaultShader.FromType(typeof(T), ShaderType.Vertex),
+                DefaultShader.FromType(typeof(T), ShaderType.Fragment));
+
+            this.vertexAttributes = vertexAttributes ?? DefaultVertexDefinition.FromType(typeof(T));
+        }
+
+        public abstract void Draw(IResourceSet resourceSet);
+    }
+}
