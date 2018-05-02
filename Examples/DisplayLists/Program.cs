2d5f9b33af98b91571b275e5cc80027d6b92c6e3
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2018-05-02 18:10:30 +0200

    Add example for testing display lists

---
+++ Examples/DisplayLists/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DisplayLists
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            new DisplayListExample().Run();
+        }
+    }
+}
