5a0ab2147256c24c68e572b48f9200519d85811d
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2018-05-02 18:20:06 +0200

    StaticDisplayList & example

--- Examples/DisplayLists/DisplayListExample.cs
+++ Examples/DisplayLists/DisplayListExample.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using PlainCore.Graphics;
+using PlainCore.Graphics.Core;
+using System;
 using System.Collections.Generic;
+using System.Numerics;
 using System.Text;
 
 namespace DisplayLists
@@ -8,7 +11,33 @@ namespace DisplayLists
     {
         public void Run()
         {
+            var window = new RenderWindow();
 
+            var dl = new StaticDisplayList<VertexPositionColor>(vertexArray, indexArray, VertexPositionColor.Size);
+            var rs = new DefaultResourceSet(window);
+
+            while (window.IsOpen)
+            {
+                window.PollEvents();
+
+                window.Clear(Color4.Black);
+                dl.Draw(rs);
+                window.Display();
+            }
+
+            window.Dispose();
         }
+
+        private static readonly VertexPositionColor[] vertexArray = new VertexPositionColor[] {
+            new VertexPositionColor(new Vector2(0.0f, 0.0f), Color4.Blue),
+            new VertexPositionColor(new Vector2(400.0f, 0.0f), Color4.Red),
+            new VertexPositionColor(new Vector2(400.0f, 400.0f), Color4.Green),
+            new VertexPositionColor(new Vector2(0.0f, 400.0f), Color4.Yellow)
+        };
+
+        private static readonly int[] indexArray = new int[]
+        {
+            0, 1, 2, 0, 2, 3
+        };
     }
 }
--- Source/PlainCore.Graphics/StaticDisplayList.cs
+++ Source/PlainCore.Graphics/StaticDisplayList.cs
@@ -1,14 +1,49 @@
-﻿using System;
+﻿using PlainCore.Graphics.Core;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace PlainCore.Graphics
 {
-    public class StaticDisplayList : IDisplayList
+    public class StaticDisplayList<T> :AbstractDisplayList<T> where T: struct
     {
-        public void Draw(IResourceSet resourceSet)
+        private readonly VertexArrayBuffer<T> vertexArrayBuffer;
+        private readonly IndexBuffer<T> indexBuffer;
+        private readonly VertexArrayObject<T> vertexArrayObject;
+        private readonly int elementCount;
+
+        public StaticDisplayList(T[] vertices, int[] indices, uint vertexSize, ShaderPipeline pipeline = null, VertexAttributeDescription[] attributes = null, OpenGL.PrimitiveType primitiveType = OpenGL.PrimitiveType.Triangles): base(pipeline, attributes)
         {
-            throw new NotImplementedException();
+            vertexArrayBuffer = new VertexArrayBuffer<T>(vertexSize, OpenGL.BufferUsage.StaticDraw, primitiveType);
+            indexBuffer = new IndexBuffer<T>(OpenGL.BufferUsage.StaticDraw);
+            vertexArrayObject = new VertexArrayObject<T>(vertexArrayBuffer, this.pipeline, vertexAttributes);
+            vertexArrayBuffer.Bind();
+            indexBuffer.Bind();
+            vertexArrayBuffer.CopyData(vertices);
+            indexBuffer.CopyData(indices);
+            indexBuffer.Unbind();
+            vertexArrayBuffer.Unbind();
+            elementCount = indices.Length;
+        }
+
+        public override void Draw(IResourceSet resourceSet)
+        {
+            pipeline.Bind();
+            vertexArrayObject.Bind();
+            indexBuffer.Bind();
+            vertexArrayBuffer.Bind();
+
+            foreach (var uniform in resourceSet.GetUniforms())
+            {
+                uniform.Set(pipeline);
+            }
+
+            indexBuffer.DrawIndexed(vertexArrayBuffer, elementCount);
+
+            vertexArrayBuffer.Unbind();
+            indexBuffer.Unbind();
+            vertexArrayObject.Unbind();
+            pipeline.Unbind();
         }
     }
 }
