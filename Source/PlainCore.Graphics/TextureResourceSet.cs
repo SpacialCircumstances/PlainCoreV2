6729d7b486ef0b5b754df9cfcddbc2a1ffc08172
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2018-05-02 17:58:47 +0200

    Add TextureResourceSet

---
+++ Source/PlainCore.Graphics/TextureResourceSet.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using PlainCore.Graphics.Core;
+
+namespace PlainCore.Graphics
+{
+    public class TextureResourceSet: DefaultResourceSet
+    {
+        public TextureResourceSet(IRenderTarget renderTarget) : base(renderTarget)
+        {
+        }
+
+        public Texture Texture { get; set; }
+
+        public override IEnumerator<IUniform> GetUniforms()
+        {
+            yield return Texture;
+            base.GetUniforms();
+        }
+    }
+}
