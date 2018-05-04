d5da78f7e054acf40c6f7d4665afaa39c5daf951
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2018-05-04 16:56:52 +0200

    Add StreamDisplayList

---
+++ Source/PlainCore.Graphics/StreamDisplayList.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using PlainCore.Graphics.Core;
+
+namespace PlainCore.Graphics
+{
+    public class StreamDisplayList<T> : AbstractDisplayList<T> where T: struct
+    {
+        private readonly VertexArrayBuffer<T> vertexArrayBuffer;
+        private readonly IndexBuffer<T> indexBuffer;
+        private readonly VertexArrayObject<T> vertexArrayObject;
+
+        public const uint DEFAULT_BUFFER_SIZE = 1024;
+
+        //Current indices count
+        private int elements;
+
+        public StreamDisplayList(uint vertexSize, OpenGL.PrimitiveType primitiveType = OpenGL.PrimitiveType.Triangles, uint vertexBufferSize = DEFAULT_BUFFER_SIZE, uint indexBufferSize = 1024, ShaderPipeline pipeline = null, Core.VertexAttributeDescription[] vertexAttributes = null) : base(pipeline, vertexAttributes)
+        {
+            vertexArrayBuffer = new VertexArrayBuffer<T>(vertexSize, OpenGL.BufferUsage.StaticDraw, primitiveType);
+            indexBuffer = new IndexBuffer<T>(OpenGL.BufferUsage.StaticDraw);
+            vertexArrayObject = new VertexArrayObject<T>(vertexArrayBuffer, this.pipeline, vertexAttributes);
+            vertexArrayBuffer.Bind();
+            indexBuffer.Bind();
+            vertexArrayBuffer.CopyRawData(IntPtr.Zero, vertexSize * vertexBufferSize);
+            indexBuffer.CopyRawData(IntPtr.Zero, sizeof(int) * indexBufferSize);
+            indexBuffer.Unbind();
+            vertexArrayBuffer.Unbind();
+        }
+
+        public void SetIndices(int[] indices)
+        {
+            elements = indices.Length;
+            indexBuffer.Bind();
+            indexBuffer.ReplaceData(indices, IntPtr.Zero);
+            indexBuffer.Unbind();
+        }
+
+        public void SetVertices(T[] vertices)
+        {
+            vertexArrayBuffer.Bind();
+            vertexArrayBuffer.ReplaceData(vertices, IntPtr.Zero);
+            vertexArrayBuffer.Unbind();
+        }
+
+        public override void Draw(IResourceSet resourceSet)
+        {
+            pipeline.Bind();
+            vertexArrayObject.Bind();
+            indexBuffer.Bind();
+            vertexArrayBuffer.Bind();
+
+            foreach (var uniform in resourceSet.GetUniforms())
+            {
+                uniform.Set(pipeline);
+            }
+
+            indexBuffer.DrawIndexed(vertexArrayBuffer, elements);
+
+            vertexArrayBuffer.Unbind();
+            indexBuffer.Unbind();
+            vertexArrayObject.Unbind();
+            pipeline.Unbind();
+        }
+    }
+}
