6cdb007c825d88af75a25a11163276fff1661550
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2018-05-06 19:13:23 +0200

    Add renderer interface

---
+++ Source/PlainCore.Graphics/IRenderer.cs
@@ -0,0 +1,17 @@
+﻿using PlainCore.Graphics.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlainCore.Graphics
+{
+    public interface IRenderer<T>
+    {
+        int[] Indices { get; }
+        T[] Vertices { get; }
+        uint VertexSize { get; }
+        OpenGL.PrimitiveType Primitive { get; }
+        ShaderPipeline Shader { get; }
+        VertexAttributeDescription[] VertexAttributes { get; }
+    }
+}
