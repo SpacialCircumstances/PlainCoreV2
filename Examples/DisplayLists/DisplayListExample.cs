cf44958e770dcbbc88386ce06d59f44fc53621eb
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2018-05-07 19:30:47 +0200

    Use shape renderer in example

--- Examples/DisplayLists/DisplayListExample.cs
+++ Examples/DisplayLists/DisplayListExample.cs
@@ -1,5 +1,6 @@
 ﻿using PlainCore.Graphics;
 using PlainCore.Graphics.Core;
+using PlainCore.Graphics.Shapes;
 using System;
 using System.Collections.Generic;
 using System.Numerics;
@@ -13,17 +14,24 @@ namespace DisplayLists
         {
             var window = new RenderWindow();
 
+            var renderer = new ShapeRenderer();
             var dl = new StreamDisplayList<VertexPositionColor>(VertexPositionColor.Size);
-            dl.SetIndices(indexArray);
-            dl.SetVertices(vertexArray);
+            renderer.UseDisplayList(dl);
+            var sq1 = new Rectangle(new Vector2(100f, 100f), new Vector2(200f, 200f), Color4.White);
+            var c1 = new Circle(12, new Vector2(500, 500), 60f, Color4.Red);
             var rs = new DefaultResourceSet(window);
 
             while (window.IsOpen)
             {
-                window.PollEvents();
-
                 window.Clear(Color4.Black);
+
+                window.PollEvents();
+                renderer.Begin();
+                renderer.Render(c1);
+                renderer.Render(sq1);
+                renderer.End();
                 dl.Draw(rs);
+
                 window.Display();
             }
 
