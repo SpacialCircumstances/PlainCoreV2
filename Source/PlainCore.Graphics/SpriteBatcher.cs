55368a477b090ae342ddd8f5de078e297c66821e
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2018-05-25 15:21:17 +0200

    SpriteBatcher only converts sprites to render data, leaving the container managment to the user

--- Source/PlainCore.Graphics/SpriteBatcher.cs
+++ Source/PlainCore.Graphics/SpriteBatcher.cs
@@ -1,59 +1,47 @@
 ﻿using PlainCore.Graphics.Core;
-using PlainCore.System;
 using System;
 using System.Numerics;
 
 namespace PlainCore.Graphics
 {
-    public class SpriteBatcher
+    public static class SpriteBatcher
     {
-        private readonly FastList<SpriteRenderItem> spriteItems = new FastList<SpriteRenderItem>(128);
-
-        private SpriteRenderItem[] cachedRenderItems = null;
-
-        public SpriteRenderItem[] Sprites => cachedRenderItems;
-
-        public void Begin()
+        public static SpriteRenderItem Draw(Sprite sprite)
         {
-            spriteItems.Clear();
+            return Draw(sprite.Texture, sprite.Color, sprite.Position.X, sprite.Position.Y, sprite.Size.X, sprite.Size.Y, sprite.Rotation, sprite.Origin.X, sprite.Origin.Y);
         }
 
-        public void Draw(Sprite sprite)
+        public static SpriteRenderItem Draw(ITexture texture, Color4 color, float x, float y, float width, float height, float rotation, float originX, float originY)
         {
-            Draw(sprite.Texture, sprite.Color, sprite.Position.X, sprite.Position.Y, sprite.Size.X, sprite.Size.Y, sprite.Rotation, sprite.Origin.X, sprite.Origin.Y);
+            return Draw(texture, color, x, y, width, height, rotation, originX, originY, 0, 0, 1, 1);
         }
 
-        public void Draw(ITexture texture, Color4 color, float x, float y, float width, float height, float rotation, float originX, float originY)
+        public static SpriteRenderItem Draw(ITexture texture, float x, float y, float width, float height, float rotation)
         {
-            Draw(texture, color, x, y, width, height, rotation, originX, originY, 0, 0, 1, 1);
+            return Draw(texture, Color4.White, x, y, width, height, rotation);
         }
 
-        public void Draw(ITexture texture, float x, float y, float width, float height, float rotation)
+        public static SpriteRenderItem Draw(ITexture texture, Color4 color, float x, float y, float width, float height, float rotation)
         {
-            Draw(texture, Color4.White, x, y, width, height, rotation);
+            return Draw(texture, color, x, y, width, height, rotation, 0.5f, 0.5f, 0, 0, 1, 1);
         }
 
-        public void Draw(ITexture texture, Color4 color, float x, float y, float width, float height, float rotation)
+        public static SpriteRenderItem Draw(ITexture texture, float x, float y, float width, float height)
         {
-            Draw(texture, color, x, y, width, height, rotation, 0.5f, 0.5f, 0, 0, 1, 1);
+            return Draw(texture, Color4.White, x, y, width, height, 0, 0, 1, 1);
         }
 
-        public void Draw(ITexture texture, float x, float y, float width, float height)
+        public static SpriteRenderItem Draw(ITexture texture, float x, float y)
         {
-            Draw(texture, Color4.White, x, y, width, height, 0, 0, 1, 1);
+            return Draw(texture, Color4.White, x, y);
         }
 
-        public void Draw(ITexture texture, float x, float y)
+        public static SpriteRenderItem Draw(ITexture texture, Color4 color, float x, float y)
         {
-            Draw(texture, Color4.White, x, y);
+            return Draw(texture, color, x, y, texture.Texture.Width, texture.Texture.Height, 0, 0, 1, 1);
         }
 
-        public void Draw(ITexture texture, Color4 color, float x, float y)
-        {
-            Draw(texture, color, x, y, texture.Texture.Width, texture.Texture.Height, 0, 0, 1, 1);
-        }
-
-        public void Draw(ITexture texture, Color4 color, float x, float y, float width, float height, float texX1, float texY1, float texX2, float texY2)
+        public static SpriteRenderItem Draw(ITexture texture, Color4 color, float x, float y, float width, float height, float texX1, float texY1, float texX2, float texY2)
         {
             float w = width;
             float h = height;
@@ -63,22 +51,21 @@ namespace PlainCore.Graphics
             float lowerY = texture.Rectangle.Position.Y + (texY1 * texture.Rectangle.End.Y);
             float upperY = texY2 * texture.Rectangle.End.Y;
 
-            spriteItems.Add(new SpriteRenderItem()
+            return new SpriteRenderItem()
             {
                 LD = new VertexPositionColorTexture(new Vector2(x, y), color, new Vector2(lowerX, upperY)),
                 LT = new VertexPositionColorTexture(new Vector2(x, y + h), color, new Vector2(lowerX, lowerY)),
                 RD = new VertexPositionColorTexture(new Vector2(x + w, y), color, new Vector2(upperX, lowerY)),
                 RT = new VertexPositionColorTexture(new Vector2(x + w, y + h), color, new Vector2(upperX, upperY)),
                 Texture = texture.Texture
-            });
+            };
         }
 
-        public void Draw(ITexture texture, Color4 color, float x, float y, float width, float height, float rotation, float originX, float originY, float texX1, float texY1, float texX2, float texY2)
+        public static SpriteRenderItem Draw(ITexture texture, Color4 color, float x, float y, float width, float height, float rotation, float originX, float originY, float texX1, float texY1, float texX2, float texY2)
         {
             if (rotation == 0)
             {
-                Draw(texture, color, x, y, width, height, texX1, texY1, texX2, texY2);
-                return;
+                return Draw(texture, color, x, y, width, height, texX1, texY1, texX2, texY2);
             }
 
             float lowerX = texture.Rectangle.Position.X + (texX1 * texture.Rectangle.End.X);
@@ -106,21 +93,14 @@ namespace PlainCore.Graphics
             var rdx = ((width - ox) * m11) + ((height - oy) * m12) + x;
             var rdy = ((width - ox) * m21) + ((height - oy) * m22) + y;
 
-            spriteItems.Add(new SpriteRenderItem()
+            return new SpriteRenderItem()
             {
                 LT = new VertexPositionColorTexture(new Vector2(ldx, ldy), color, new Vector2(lowerX, lowerY)),
                 RT = new VertexPositionColorTexture(new Vector2(rux, ruy), color, new Vector2(upperX, lowerY)),
                 LD = new VertexPositionColorTexture(new Vector2(rdx, rdy), color, new Vector2(upperX, upperY)),
                 RD = new VertexPositionColorTexture(new Vector2(lux, luy), color, new Vector2(lowerX, upperY)),
                 Texture = texture.Texture
-            });
-        }
-
-        public SpriteRenderItem[] End()
-        {
-            cachedRenderItems = new SpriteRenderItem[spriteItems.Count];
-            Array.Copy(spriteItems.Buffer, cachedRenderItems, spriteItems.Count);
-            return cachedRenderItems;
+            };
         }
     }
 }
