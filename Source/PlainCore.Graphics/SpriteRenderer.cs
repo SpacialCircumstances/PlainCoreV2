ae1a6d616b088a52627837b4b2f0dbcefda230c6
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2018-05-25 12:44:28 +0200

    Add simple sprite render implementation

--- Source/PlainCore.Graphics/SpriteRenderer.cs
+++ Source/PlainCore.Graphics/SpriteRenderer.cs
@@ -23,9 +23,49 @@ namespace PlainCore.Graphics
             Array.Sort(this.renderItems, renderItemsIndex, renderItemsCount);
         }
 
-        public void Render(Action<VertexPositionColorTexture[], int[], Texture> renderCallback)
+        public void RenderToData(Action<VertexPositionColorTexture[], int[], Texture> renderCallback)
         {
+            int index = renderItemsIndex;
+            int count = renderItemsCount;
 
+            unsafe
+            {
+                Texture texture = null;
+                int currentBatchCount = 0;
+                int batchStart = 0;
+
+                for (int i = index; i < count; i++)
+                {
+                    var item = renderItems[i];
+
+                    if (texture != item.Texture)
+                    {
+                        if (currentBatchCount != 0)
+                        {
+                            //Flush
+                            VertexPositionColorTexture[] vertexArray = new VertexPositionColorTexture[currentBatchCount * 4];
+                            for (int j = batchStart; j < currentBatchCount; j++)
+                            {
+                                var currentItem = renderItems[j];
+                                int vertexIndex = j * 4;
+                                vertexArray[vertexIndex] = currentItem.LT;
+                                vertexArray[vertexIndex + 1] = currentItem.RT;
+                                vertexArray[vertexIndex + 2] = currentItem.LD;
+                                vertexArray[vertexIndex + 3] = currentItem.RD;
+                            }
+
+                            renderCallback.Invoke(vertexArray, null, texture);
+
+                            currentBatchCount = 0;
+                            batchStart = i;
+                        }
+
+                        texture = item.Texture;
+                    }
+
+                    currentBatchCount++;
+                }
+            }
         }
 
         public uint VertexSize => VertexPositionColorTexture.Size;
