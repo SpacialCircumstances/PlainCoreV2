dbc940191cddc8662b6bcedbea4f3e73e7bcf95a
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2018-06-02 13:33:37 +0200

    Add Layer to SpriteRenderItem and use it for comparing

--- Source/PlainCore.Graphics/SpriteRenderItem.cs
+++ Source/PlainCore.Graphics/SpriteRenderItem.cs
@@ -8,26 +8,27 @@ namespace PlainCore.Graphics
     /// </summary>
     public struct SpriteRenderItem: IComparable<SpriteRenderItem>
     {
-
-
         public VertexPositionColorTexture LT;
         public VertexPositionColorTexture RT;
         public VertexPositionColorTexture RD;
         public VertexPositionColorTexture LD;
         public Texture Texture;
+        public int Layer;
+        public int SortKey => (Layer * 256) + (int)Texture.InternalTexture.Handle;
 
-        public SpriteRenderItem(VertexPositionColorTexture lT, VertexPositionColorTexture rT, VertexPositionColorTexture rD, VertexPositionColorTexture lD, Texture texture)
+        public SpriteRenderItem(VertexPositionColorTexture lT, VertexPositionColorTexture rT, VertexPositionColorTexture rD, VertexPositionColorTexture lD, Texture texture, int layer = 0)
         {
             LT = lT;
             RT = rT;
             RD = rD;
             LD = lD;
             Texture = texture;
+            Layer = layer;
         }
 
         public int CompareTo(SpriteRenderItem other)
         {
-            return (int)Texture.InternalTexture.Handle - (int)other.Texture.InternalTexture.Handle;
+            return SortKey - other.SortKey;
         }
     }
 }
